a3bcc93153aa9d535c96af4c5b8b329c32a21c1c
reaver <ragontar@gmail.com>
2022-11-17 13:49:09 +0300

    env initialization

--- transaction-system-service/env/envs.go
+++ transaction-system-service/env/envs.go
@@ -1,8 +1,31 @@
 package env
 
-const (
-	DB_USER = "postgres"
-	DB_PASSWORD = "postgres"
-	DB_ADDR = "db"
-	DB_DATABASE = "postgres"
+import (
+	"fmt"
+	"os"
 )
+
+var (
+	DB_USER string
+	DB_PASSWORD string
+	DB_ADDR string
+	DB_DATABASE string
+)
+
+// TODO read from environment
+
+func init() {
+	var ok bool
+	if DB_USER, ok = os.LookupEnv("DB_USER"); !ok {
+		panic(fmt.Sprintf("Cannot lookup %s", "DB_USER"))
+	}
+	if DB_PASSWORD, ok = os.LookupEnv("DB_PASSWORD"); !ok {
+		panic(fmt.Sprintf("Cannot lookup %s", "DB_PASSWORD"))
+	}
+	if DB_ADDR, ok = os.LookupEnv("DB_ADDR"); !ok {
+		panic(fmt.Sprintf("Cannot lookup %s", "DB_ADDR"))
+	}
+	if DB_DATABASE, ok = os.LookupEnv("DB_DATABASE"); !ok {
+		panic(fmt.Sprintf("Cannot lookup %s", "DB_DATABASE"))
+	}
+}
